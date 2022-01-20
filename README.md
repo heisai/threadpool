b516517f7c0707dcdf04b86c93bee23a35c1acec
sen.yang <1049870.53@qq.com>
2022-01-20 10:58:35 +0800

    增加添加队列方法

--- README.md
+++ README.md
@@ -12,15 +12,43 @@
         std::vector<std::future<int>>vec;
  -  添加任务
 
+        
+
+        // 案例1 lambda表达式：
         for(int i =0;i<20;i++)
         {
 
-        vec.emplace_back(
-                    pool.AddTask([i]{
-                    std::this_thread::sleep_for(std::chrono::seconds(5));
-                    return i*i;
-        }));
-    }
+            vec.emplace_back(
+                        pool.AddTask([i]{
+                        std::this_thread::sleep_for(std::chrono::seconds(5));
+                        return i*i;
+            }));
+        }
+
+        // 案例2:（普通函数，）
+        for(int i =0;i<20;i++)
+        {
+            auto taskfun = pool.AddTask(task,123 + i,"data");
+            vec.emplace_back(std::move(taskfun));
+        }
+
+        // 案例3:（类成员函数）
+
+        TestObj obj;
+        for(int i =0;i<20;i++)
+        {
+            std::function<int(int,string)> func = bind(&TestObj::task,&obj,placeholders::_1,placeholders::_2);
+            auto taskfun = pool.AddTask(func,123+i,"1234567");
+            vec.emplace_back(std::move(taskfun));
+        }
+        // 案例4:（静态函数）
+
+        for(int i =0;i<20;i++)
+        {
+            std::function<int(int,string)> func = bind(&TestObj::taskstatic,placeholders::_1,placeholders::_2);
+            auto taskfun = pool.AddTask(func,123+i,"1234567");
+            vec.emplace_back(std::move(taskfun));
+        }
 
 - 获取返回值 //没有返回值 可忽略
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include"threadpool.h"
 
+
+
+int task(int num,const std::string &data)
+{
+    return num + data.size();
+}
+
+class TestObj
+{
+public:
+    int task(int num,const std::string &data)
+    {
+        return num + data.size();
+    }
+    static int taskstatic(int num,const std::string &data)
+    {
+        return num + data.size();
+    }
+};
 int main()
 {
     cout << "Hello World!" << endl;
     std::vector<std::future<int>>vec;
     ThreadPool pool(4);
+    // 案例1 lambda表达式：
     for(int i =0;i<20;i++)
     {
 
@@ -14,6 +34,29 @@ int main()
                     return i*i;
         }));
     }
+    // 案例2:（普通函数，）
+    for(int i =0;i<20;i++)
+    {
+        auto taskfun = pool.AddTask(task,123 + i,"data");
+        vec.emplace_back(std::move(taskfun));
+    }
+
+    // 案例3:（类成员函数）
+    TestObj obj;
+    for(int i =0;i<20;i++)
+    {
+        std::function<int(int,string)> func = bind(&TestObj::task,&obj,placeholders::_1,placeholders::_2);
+        auto taskfun = pool.AddTask(func,123+i,"1234567");
+        vec.emplace_back(std::move(taskfun));
+    }
+     // 案例3:（静态函数）
+    for(int i =0;i<20;i++)
+    {
+        std::function<int(int,string)> func = bind(&TestObj::taskstatic,placeholders::_1,placeholders::_2);
+        auto taskfun = pool.AddTask(func,123+i,"1234567");
+        vec.emplace_back(std::move(taskfun));
+    }
+
     for(auto &&result:vec)
     {
         std::cout<<"get_data:" << result.get()<<std::endl;
